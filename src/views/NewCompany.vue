1685d98fe3642f6e3a2737db9e836af6953c31f4
VanyaKrylov <studentik7@yandex.ru>
2020-12-29 02:02:28 +0300

    DONE

--- src/views/NewCompany.vue
+++ src/views/NewCompany.vue
@@ -1,15 +1,55 @@
 <template>
-<div>
-  <h1>Create new company</h1>
-</div>
+  <div>
+    <h1>Create new company</h1>
+    <form class="create-company" @submit.prevent="submitNew">
+      <label for="name">Name</label>
+      <input type="text" id="name" placeholder="Name" v-model="companyName" />
+      <label for="capital">Capital</label>
+      <input type="text" id="capital" placeholder="sum" v-model="capital" />
+      <input
+        type="password"
+        id="Password"
+        placeholder="Password"
+        v-model="password"
+      />
+      <button type="submit">Create</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import * as axios from "axios";
+
 export default {
-  name: "NewCompany"
-}
+  name: "NewCompany",
+  data() {
+    return {
+      companyName: "",
+      capital: "",
+      password: ""
+    };
+  },
+  methods: {
+    submitNew() {
+      axios({
+        method: "post",
+        url: "http://localhost:8181/sign-up/company",
+        data: {
+          name: this.companyName,
+          capital: this.capital,
+          password: this.password
+        }
+      })
+        .then(response => {
+          console.log(response);
+          this.$router.push("/");
+        })
+        .catch(e => {
+          alert(e.response.data.error);
+        });
+    }
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
     <h1>Hello, new user</h1>
-    <h1>{{ this.$store.state.token }}</h1>
-    <ul>
-      <li v-for="broker in brokers" :key="broker">{{ broker.name + ':' + broker.capital }}</li>
-    </ul>
+    <form class="new-user" @submit.prevent="newUser">
+      <label for="name">Name</label>
+      <input type="text" id="name" placeholder="Name" v-model="userName" />
+      <label for="surname">Surname</label>
+      <input type="text" id="surname" placeholder="Surname" v-model="surname" />
+      <label for="capital">Money</label>
+      <input type="text" id="capital" placeholder="Capital" v-model="capital" />
+      <select v-model="broker">
+        <option v-for="b in brokers" :key="b.id" v-bind:value="b">{{
+          b.name + ":" + b.capital
+        }}</option>
+      </select>
+      <input
+        type="password"
+        id="Password"
+        placeholder="Password"
+        v-model="password"
+      />
+      <button type="submit">Create</button>
+    </form>
   </div>
 </template>
 
@@ -14,8 +30,35 @@ import * as axios from "axios";
 export default {
   name: "NewUser",
   data: () => ({
-    brokers: []
+    brokers: [],
+    broker: "",
+    userName: "",
+    surname: "",
+    capital: "",
+    password: ""
   }),
+  methods: {
+    newUser() {
+      axios({
+        method: "post",
+        url: "http://localhost:8181/sign-up/user",
+        data: {
+          name: this.userName,
+          surname: this.surname,
+          broker: this.broker,
+          capital: this.capital,
+          password: this.password
+        }
+      })
+        .then(response => {
+          console.log(response);
+          this.$router.push("/");
+        })
+        .catch(e => {
+          alert(e.response.data.error);
+        });
+    }
+  },
   created() {
     axios
       .get("http://localhost:8181/api/v0/broker/all")
